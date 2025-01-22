6a10f89446935443d7ab78a620936a4d3d6a94b9
root <root@instance-20241207-192655.africa-south1-a.c.responsive-lens-443912-v6.internal>
2025-01-22 00:58:08 +0000

    update

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -86,12 +86,12 @@ bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
                         <span class="">Create a post</span>
                     </a>
                     </li>
-                    {{--<li>
+                    <li>
                       <a href="{{ route('login.qrcode') }}" class="  block py-2 px-3 text-gray-900 rounded hover:bg-blue-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                         <!-- Plus icon -->
                         Create campaign 
                       </a>
-                    </li>
+                    {{--</li>
                     <li>
                       <a href="{{ route('my.profile') }}" class="  block py-2 px-3 text-gray-900 rounded hover:bg-blue-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                         <!-- Plus icon -->
