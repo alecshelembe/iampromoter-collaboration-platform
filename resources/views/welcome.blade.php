4c853651a0e542beb57fc69ec90c244e9d49f5cb
alecshelembe <alecshelembe@gmail.com>
2026-01-09 20:28:26 +0200

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,7 +26,7 @@
 
     <title>@more_collabz</title>
 
-    <link rel="canonical" href="http://127.0.0.1/"">
+    <link rel="canonical" href="http://127.0.0.1/">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
